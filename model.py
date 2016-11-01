5afb43897d776af965b3380061ed41a188bb8428
Michelle Kim <jmichkim@gmail.com>
2016-11-01 07:40:19 +0000

    implemented some of the form and got a suggested location to show up below the form

--- model.py
+++ model.py
@@ -124,6 +124,7 @@ def connect_to_db(app):
 
     # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///meatup'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import googlemaps
+import requests
+import yelp
 
 from jinja2 import StrictUndefined
 
@@ -28,7 +30,83 @@ gmaps = googlemaps.Client(key=os.environ['GMAPS_KEY'])
 @app.route('/')
 def index():
     """ I have only the form so far, so it'll be the homepage. """
-    return render_template("form.html")
+
+    if not request.args.get('submitting-search-form'):
+        return render_template("form.html", result=None)
+    else:
+        # Get form variables
+        term1 = request.args.get("term1")
+        st_address1 = request.args.get("st_address1")
+        city1 = request.args.get("city1")
+        state1 = request.args.get("state1")
+        radius1 = request.args.get("radius1")
+        categories1 = request.args.get("categories1")
+
+        term2 = request.args.get("term2")
+        st_address2 = request.args.get("st_address2")
+        city2 = request.args.get("city2")
+        state2 = request.args.get("state2")
+        radius2 = request.args.get("radius2")
+        categories2 = request.args.get("categories2")
+
+        # should I replace this with a for loop for variable # of people to meet up?
+
+        latitude, longitude = midpt_formula(combine_coordinates_for_midpt(geocoding(st_address1, city1, state1), geocoding(st_address2, city2, state2)))
+
+        params_midpt = {'term': term1 + ", " + term2,
+                        'latitude': latitude,
+                        'longitude': longitude,
+                        'radius': stricter_radius(radius1, radius2)}
+
+        params_user1 = {'term': term1,
+                        'latitude': geocoding(st_address1, city1, state1)[0],
+                        'longitude': geocoding(st_address1, city1, state1)[1],
+                        'radius': radius1}
+
+        params_user2 = {'term': term2,
+                        'latitude': geocoding(st_address2, city2, state2)[0],
+                        'longitude': geocoding(st_address2, city2, state2)[1],
+                        'radius': radius2}
+
+        url = 'https://api.yelp.com/v3/businesses/search'
+        resp = requests.get(url=url, params=params_midpt, headers={'Authorization': 'Bearer ' + os.environ['YELP_KEY']})
+        responses = resp.json()
+
+        # import pdb; pdb.set_trace()
+        name_of_first_search_result = responses['businesses'][0]['name']
+
+        # return render_template("search-results.html", name_of_first_search_result=name_of_first_search_result)
+        return render_template("form.html", result=name_of_first_search_result)
+
+
+def stricter_radius(radius1, radius2):
+    if radius1 > radius2:
+        return radius2
+    return radius1
+
+
+def geocoding(st_address, city, state):
+    """Turns the given address into a list containing latitude and longitude."""
+    geocoded_address = gmaps.geocode(", ".join([st_address, city, state]))
+    coords_list = geocoded_address[0]['geometry']['location'].values()
+    return coords_list
+
+
+def combine_coordinates_for_midpt(coords_list1, coords_list2):
+    """ Preps two coordinate pairs for the midpoint formula calculation. """
+    return coords_list1 + coords_list2
+
+
+def midpt_formula(coordinates):
+    """Takes two coordinate pairs and returns a coordinate pair that is midway."""
+    xm = (coordinates[0] + coordinates[2])/2.0
+    ym = (coordinates[1] + coordinates[3])/2.0
+    return [xm, ym]
+
+# @app.route('/search-results')
+# def show_search_results():
+    # return render_template("search-results.html")
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
@@ -40,4 +118,4 @@ if __name__ == "__main__":
     # Use the DebugToolbar
     DebugToolbarExtension(app)
 
-    app.run(port=5000)
+    app.run(host="0.0.0.0")
