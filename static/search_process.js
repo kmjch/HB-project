1fe5ac78ba7564903b6dd63d591f5fcb7b063e82
Michelle <jhmichellekim@gmail.com>
2016-11-03 16:31:26 -0700

    got map to show user's current location

--- static/search_process.js
+++ static/search_process.js
@@ -1,16 +1,38 @@
 function initMap() {
-        var myLatLng = {lat: 37.7886679, lng: -122.4114987};
+  var myLatLng = {lat: 37.7886679, lng: -122.4114987};
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 16,
-          center: myLatLng
-        });
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: myLatLng
+  });
 
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-        });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+// for geolocation to work, have to run on localhost rather than 0.0.0.0
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
 
 
