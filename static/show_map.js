e2fd80a88662b14ae3c2f253f97298d11ede17bf
Michelle <jhmichellekim@gmail.com>
2016-11-02 18:02:27 -0700

    got marker to show thanks google maps documentation

--- static/show_map.js
+++ static/show_map.js
@@ -1,14 +1,14 @@
-var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 37.7886679, lng: -122.4114987},
-          // how to pass in variables to javascript files?
-          zoom: 16
-        });
-      }
+function initMap() {
+        var myLatLng = {lat: 37.7886679, lng: -122.4114987};
 
-function addMarker() {
-  var myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-  var image = myImageURL;
-  var hackbright = new google.maps.LatLng(37.7886679, -122.4114987);
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+          position: myLatLng,
+          map: map,
+          title: 'Hello World!'
+        });
 }
