0a9f3f439ee78c523b5da1ee050bc9a38e3b162e
Michelle <jhmichellekim@gmail.com>
2016-11-16 17:03:57 -0800

    removed autocomplete and back to normal

--- static/search_process.js
+++ static/search_process.js
@@ -1,11 +1,6 @@
 var map, openNow;
 var markers = [];
 
-function initMapAndAuto() {
-  initMap();
-  initAutocomplete();
-}
-
 // toggles the form input for midpoint or venn diagram
 $('#search-type').change(changeFormType);
 var chooseType = $('#search-type').val();
@@ -31,53 +26,9 @@ function showTimeField(evt) {
   }
 }
 
-var placeSearch, autocomplete;
-var componentForm = {
-  street_number: 'short_name',
-  route: 'long_name',
-  locality: 'long_name',
-  administrative_area_level_1: 'short_name',
-  country: 'long_name',
-  postal_code: 'short_name'
-};
-
-function initAutocomplete() {
-  // Create the autocomplete object, restricting the search to geographical
-  // location types.
-  console.log("initAutocomplete");
-  autocomplete = new google.maps.places.Autocomplete(
-      /** @type {!HTMLInputElement} */(document.getElementById('cheese')),
-      {types: ['geocode']});
-
-  // When the user selects an address from the dropdown, populate the address
-  // fields in the form.
-  autocomplete.addListener('place_changed', fillInAddress);
-}
-
-function fillInAddress() {
-  // Get the place details from the autocomplete object.
-  var place = autocomplete.getPlace();
-
-  for (var component in componentForm) {
-    document.getElementById(component).value = '';
-    document.getElementById(component).disabled = false;
-  }
-
-  // Get each component of the address from the place details
-  // and fill the corresponding field on the form.
-  for (var i = 0; i < place.address_components.length; i++) {
-    var addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      var val = place.address_components[i][componentForm[addressType]];
-      document.getElementById(addressType).value = val;
-      console.log('addressType', addressType);
-    }
-  }
-}
 
 function initMap() {
   var myLatLng = {lat: 37.7886679, lng: -122.4114987};
-  console.log("hihimap");
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
     center: myLatLng
