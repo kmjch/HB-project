e8533717f1fe849fbc87fa11b251e6383025dcc9
Michelle <jhmichellekim@gmail.com>
2016-11-25 17:19:57 -0800

    got sample to show up now need to connect ml alg to psql

--- server.py
+++ server.py
@@ -277,92 +277,7 @@ def logout():
     return redirect("/")
 
 
-with open('test.csv') as csvfile:
-    res_data = pandas.read_csv(csvfile, header=0)
-# The names of all the columns in the data.
-print res_data.columns.values
 
-
-# Select gather from our dataset
-selected_restaurant = res_data[res_data['name'] == "'" + highest_rated_restaurant + "'"].iloc[0]
-# selected_restaurant = res['businesses'][0]['name']
-
-# # Choose only the numeric columns (we'll use these to compute euclidean distance)
-distance_columns = ['rating', 'price', 'review_count']
-
-# # rating: res_data['businesses'][i]['rating']
-# # price: res_data['businesses'][i]['price']
-# # review_count: res_data['businesses'][i]['review_count']
-
-# # name: res_data['businesses'][i]['name']
-# # categories: res_data['businesses'][i]['categories'][j]['alias']
-# # categories: res_data['businesses'][i]['categories'][j]['title']
-
-
-def euclidean_distance(row):
-    """A simple euclidean distance function"""
-    inner_value = 0
-    for k in distance_columns:
-        inner_value += (row[k] - selected_restaurant[k]) ** 2
-    return math.sqrt(inner_value)
-
-# Find the distance from each restaurant in the dataset to gather.
-gather_distance = res_data.apply(euclidean_distance, axis=1)
-
-# Select only the numeric columns from the restaurant res_data dataset
-res_numeric = res_data[distance_columns]
-
-# Normalize all of the numeric columns
-res_normalized = (res_numeric - res_numeric.mean()) / res_numeric.std()
-
-from scipy.spatial import distance
-
-# Fill in NA values in res_normalized
-res_normalized.fillna(0, inplace=True)
-
-# Find the normalized vector for the most highly rated restaurant.
-highest_rated_restaurant_normalized = res_normalized[res_data["name"] == "'" + highest_rated_restaurant + "'"]
-
-# Find the distance between gather and everywhere else.
-euclidean_distances = res_normalized.apply(lambda row: distance.euclidean(row, highest_rated_restaurant_normalized), axis=1)
-
-# Create a new dataframe with distances.
-distance_frame = pandas.DataFrame(data={"dist": euclidean_distances, "idx": euclidean_distances.index})
-distance_frame.sort("dist", inplace=True)
-
-# Find the most similar restaurant to gather (the lowest distance to gather is
-# gather, the second smallest is the most similar non-gather restaurant)
-second_smallest = distance_frame.iloc[1]["idx"]
-most_similar_to_highest_rated_restaurant = res_data.loc[int(second_smallest)]["name"]
-
-
-import random
-from numpy.random import permutation
-
-# Randomly shuffle the index of res_data.
-random_indices = permutation(res_data.index)
-# Set a cutoff for how many items we want in the test set (in this case 1/3 of the items)
-test_cutoff = math.floor(len(res_data)/3)
-# Generate the test set by taking the first 1/3 of the randomly shuffled indices.
-test = res_data.loc[random_indices[1:test_cutoff]]
-
-# Generate the train set with the rest of the data.
-train = res_data.loc[random_indices[test_cutoff:]]
-
-# The columns that we will be making predictions with.
-x_columns = ['rating', 'price', 'review_count']
-# The column that we want to predict.
-y_column = ["user_rating"]
-# need to make this
-
-from sklearn.neighbors import KNeighborsRegressor
-# Create the knn model.
-# Look at the five closest neighbors.
-knn = KNeighborsRegressor(n_neighbors=5)
-# Fit the model on the training data.
-knn.fit(train[x_columns], train[y_column])
-# Make point predictions on the test set using the fit model.
-predictions = knn.predict(test[x_columns])
 
 @app.route("/users/<username>")
 def user_detail(username):
@@ -374,10 +289,95 @@ def user_detail(username):
     highest_rated_restaurant = sorted_ratings[0][1]
     session['username'] = user.username
 
+    with open('test.csv') as csvfile:
+        res_data = pandas.read_csv(csvfile, header=0)
+        # change to read from psql meatup
+    # The names of all the columns in the data.
+    print res_data.columns.values
+
+    # Select gather from our dataset
+    selected_restaurant = res_data[res_data['name'] == "Gather"].iloc[0]
+    # selected_restaurant = res['businesses'][0]['name']
+
+    # # Choose only the numeric columns (we'll use these to compute euclidean distance)
+    distance_columns = ['rating', 'price', 'review_count']
+
+    # # rating: res_data['businesses'][i]['rating']
+    # # price: res_data['businesses'][i]['price']
+    # # review_count: res_data['businesses'][i]['review_count']
+
+    # # name: res_data['businesses'][i]['name']
+    # # categories: res_data['businesses'][i]['categories'][j]['alias']
+    # # categories: res_data['businesses'][i]['categories'][j]['title']
+
+    def euclidean_distance(row):
+        """A simple euclidean distance function"""
+        inner_value = 0
+        for k in distance_columns:
+            inner_value += (row[k] - selected_restaurant[k]) ** 2
+        return math.sqrt(inner_value)
+
+    # Find the distance from each restaurant in the dataset to gather.
+    gather_distance = res_data.apply(euclidean_distance, axis=1)
+
+    # Select only the numeric columns from the restaurant res_data dataset
+    res_numeric = res_data[distance_columns]
+
+    # Normalize all of the numeric columns
+    res_normalized = (res_numeric - res_numeric.mean()) / res_numeric.std()
+
+    from scipy.spatial import distance
+
+    # Fill in NA values in res_normalized
+    res_normalized.fillna(0, inplace=True)
+
+    # Find the normalized vector for the most highly rated restaurant.
+    highest_rated_restaurant_normalized = res_normalized[res_data["name"] == "Gather"]
+
+    # Find the distance between gather and everywhere else.
+    euclidean_distances = res_normalized.apply(lambda row: distance.euclidean(row, highest_rated_restaurant_normalized), axis=1)
+
+    # Create a new dataframe with distances.
+    distance_frame = pandas.DataFrame(data={"dist": euclidean_distances, "idx": euclidean_distances.index})
+    distance_frame.sort("dist", inplace=True)
+
+    # Find the most similar restaurant to gather (the lowest distance to gather is
+    # gather, the second smallest is the most similar non-gather restaurant)
+    second_smallest = distance_frame.iloc[1]["idx"]
+    most_similar_to_highest_rated_restaurant = res_data.loc[int(second_smallest)]["name"]
+
+    # import random
+    # from numpy.random import permutation
+
+    # # Randomly shuffle the index of res_data.
+    # random_indices = permutation(res_data.index)
+    # # Set a cutoff for how many items we want in the test set (in this case 1/3 of the items)
+    # test_cutoff = math.floor(len(res_data)/3)
+    # # Generate the test set by taking the first 1/3 of the randomly shuffled indices.
+    # test = res_data.loc[random_indices[1:test_cutoff]]
+
+    # # Generate the train set with the rest of the data.
+    # train = res_data.loc[random_indices[test_cutoff:]]
+
+    # # The columns that we will be making predictions with.
+    # x_columns = ['rating', 'price', 'review_count']
+    # # The column that we want to predict.
+    # y_column = ["user_rating"]
+    # # need to make this
+
+    # from sklearn.neighbors import KNeighborsRegressor
+    # # Create the knn model.
+    # # Look at the five closest neighbors.
+    # knn = KNeighborsRegressor(n_neighbors=5)
+    # # Fit the model on the training data.
+    # knn.fit(train[x_columns], train[y_column])
+    # # Make point predictions on the test set using the fit model.
+    # predictions = knn.predict(test[x_columns])
+
     return render_template("user.html",
                            user=user,
                            sorted_ratings=sorted_ratings,
-                           predictions=predictions,
+                           # predictions=predictions,
                            highest_rated=highest_rated_restaurant,
                            most_sim=most_similar_to_highest_rated_restaurant)
 
