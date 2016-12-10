7cef29ee6598f36b347acf805b7432abc989b2c7
Michelle <jhmichellekim@gmail.com>
2016-12-10 15:40:12 -0800

    imported os to model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from random import randint
-import json, datetime
+import os, json, datetime
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
